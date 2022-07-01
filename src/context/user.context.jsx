2976195cf42000cdd8ab5808a99d53726fef8d78
unknown <kousikdkush1908@gmail.com>
2022-07-01 15:02:45 +0530

    commited

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,51 +1,52 @@
-import { createContext,useEffect,useReducer } from "react";
-
-import { onAuthStateChangedListner,CreateUserdocFromAuth } from "../utils/firebase/firebase.utils";
+import { createContext, useEffect, useReducer } from "react";
+import { createAction } from '../utils/reducer/reducerutils'
+import { onAuthStateChangedListner, CreateUserdocFromAuth } from "../utils/firebase/firebase.utils";
 export const UserContext = createContext({
     currentUser: null, setCurrentUser: () => null
 })
 
 
-export const USER_ACTION_TYPES={
-    SET_CURRENT_USER:'SET_CURRENT_USER',
+export const USER_ACTION_TYPES = {
+    SET_CURRENT_USER: 'SET_CURRENT_USER',
 }
-const userReducer=(state,action)=>{
+const userReducer = (state, action) => {
     console.log(action)
-    const {type,payload}=action;
-    switch(type){
+    const { type, payload } = action;
+    switch (type) {
         case USER_ACTION_TYPES.SET_CURRENT_USER:
-            return{
+            return {
                 ...state,
-                currentUser:payload
+                currentUser: payload
             }
         default:
             throw new Error(`Unhandled exception type ${type} in useReducer`)
     }
 }
- 
-const INITIAL_STATE={
-    currentUser:null,
+
+const INITIAL_STATE = {
+    currentUser: null,
 }
 
 
 
 export const UserProvider = ({ children }) => {
     // const [currentUser, setCurrentUser] = useState(null)
-    const [{currentUser},dispatch]=useReducer(userReducer,INITIAL_STATE)
+    const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE)
     console.log(currentUser)
-    
-    const setCurrentUser=(user)=>{
-        dispatch({type:USER_ACTION_TYPES.SET_CURRENT_USER,payload:user})
+
+    const setCurrentUser = (user) => {
+        dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+
     }
 
     const value = { currentUser, setCurrentUser }
 
     useEffect(() => {
         const unsubscribe = onAuthStateChangedListner((user) => {
-            if(user){
-                 CreateUserdocFromAuth(user)
+            if (user) {
+                CreateUserdocFromAuth(user)
             }
-           setCurrentUser(user)
+            setCurrentUser(user)
         })
         return unsubscribe
     }, [])
